3d1fab87aaf6f47fdd2e55df218ec5a795f48760
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2024-01-17 15:25:03 +0000

    removed QuEST direct tests, keep in QuEST.jl package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,10 @@
-using QuESTMbqcBqpVerification
+using RobustBlindVerification
 using Test
 
 
 tolerance=1e-10
 num_iterations=1<<2
 
-include("../test/QuEST_test_functions.jl")
-include("../test/test_functions.jl")
 
-include("../test/QuEST_testsets.jl")
+include("../test/test_functions.jl")
 include("../test/testsets.jl")
