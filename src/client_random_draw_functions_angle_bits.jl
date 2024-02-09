1ecc1140d268b94e38ef457f2278dab17d0633e4
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2024-02-09 20:34:47 +0000

    added documentation

--- src/client_random_draw_functions_angle_bits.jl
+++ src/client_random_draw_functions_angle_bits.jl
@@ -6,28 +6,90 @@
 #           : number and angles
 #           :
 ##################################################################
+
+
+
+
+
+
 """
-    Draw random bit 0 or 1
+    draw_bit()
+
+Draw a random bit, either 0 or 1. This function uses the `rand` function to randomly select from the array `[0,1]`.
+
+# Returns
+- A random bit, either 0 or 1.
+
+# Examples
+```julia
+draw_bit() # Outputs: 0 or 1
+```
 """
 draw_bit() = rand([0,1])
 
+
 """
-    Draw random interger between 0 and 7
+    rand_k_0_7()
+
+Draw a random integer between 0 and 7, inclusive. This function uses the `rand` function to randomly select from the range `0:7`.
+
+# Returns
+- A random integer between 0 and 7, inclusive.
+
+# Examples
+```julia
+rand_k_0_7() # Outputs: An integer between 0 and 7
+```
 """
 rand_k_0_7() = rand(0:7)
 
+
 """
-    For draw θ a multiple of kπ/4, k ∈ 0..7
+    draw_θᵥ()
+
+Draw a random angle θ that is a multiple of kπ/4, where k is an integer between 0 and 7, inclusive. This function uses the `rand_k_0_7` function to select k, and then calculates θ.
+
+# Returns
+- A random angle θ that is a multiple of kπ/4.
+
+# Examples
+```julia
+draw_θᵥ() # Outputs: A multiple of π/4 between 0 and 7π/4
+```
 """
 draw_θᵥ() = rand_k_0_7()*π/4.0
 
+
+
 """
-    Draw random bit 0 or 1 for trap
+    draw_rᵥ()
+
+Draw a random bit, either 0 or 1, for a trap. This function uses the `draw_bit` function to select the bit.
+
+# Returns
+- A random bit, either 0 or 1, for a trap.
+
+# Examples
+```julia
+draw_rᵥ() # Outputs: 0 or 1
+```
 """
 draw_rᵥ() = draw_bit()
 
+
+
 """
-    Draw random bit 0 or 1 for dummy
+    draw_dᵥ()
+
+Draw a random bit, either 0 or 1, for a dummy. This function uses the `draw_bit` function to select the bit.
+
+# Returns
+- A random bit, either 0 or 1, for a dummy.
+
+# Examples
+```julia
+draw_dᵥ() # Outputs: 0 or 1
+````
 """
 draw_dᵥ() = draw_bit()
 
