1af095c0a5992c09b8c7a2b4182aa0f99113487d
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2024-02-12 15:40:45 +0000

    added local

--- src/client_update_angle.jl
+++ src/client_update_angle.jl
@@ -222,6 +222,26 @@ end
 
 
 
+"""
+    update_ϕ!(::TestRound, ::DummyQubit, ::NoInputQubits, meta_graph, vertex)
+
+Updates the ϕ value of a given vertex in the `meta_graph` during a test round with a dummy qubit and no input qubits. The new ϕ value is computed based on a randomly drawn θᵥ value.
+
+# Arguments
+- `::TestRound`: Indicates that this function is used during a test round.
+- `::DummyQubit`: Indicates that a dummy qubit is used.
+- `::NoInputQubits`: Indicates that there are no input qubits.
+- `meta_graph`: The graph to be updated.
+- `vertex`: The vertex in the graph for which the ϕ value is to be updated.
+
+# Returns
+- The updated `meta_graph`.
+
+# Examples
+```julia
+updated_graph = update_ϕ!(TestRound(), DummyQubit(), NoInputQubits(), meta_graph, vertex) # Updates the ϕ value of the specified vertex in the graph
+```
+"""
 function update_ϕ!(::TestRound,::DummyQubit,::NoInputQubits,meta_graph,vertex)
     θᵥ = draw_θᵥ()
     ϕ̃ = compute_angle_δᵥ(TestRound(),DummyQubit(),θᵥ)
@@ -229,9 +249,27 @@ function update_ϕ!(::TestRound,::DummyQubit,::NoInputQubits,meta_graph,vertex)
     return meta_graph
 end
 
+"""
+    update_ϕ!(::TestRound, ::TrapQubit, ::NoInputQubits, meta_graph, vertex)
 
+Updates the ϕ value of a given vertex in the `meta_graph` during a test round with a trap qubit and no input qubits. The new ϕ value is computed based on the initial qubit θᵥ and a randomly drawn rᵥ value.
+
+# Arguments
+- `::TestRound`: Indicates that this function is used during a test round.
+- `::TrapQubit`: Indicates that a trap qubit is used.
+- `::NoInputQubits`: Indicates that there are no input qubits.
+- `meta_graph`: The graph to be updated.
+- `vertex`: The vertex in the graph for which the ϕ value is to be updated.
+
+# Returns
+- The updated `meta_graph`.
+
+# Examples
+```julia
+updated_graph = update_ϕ!(TestRound(), TrapQubit(), NoInputQubits(), meta_graph, vertex) # Updates the ϕ value of the specified vertex in the graph
+```
+"""
 function update_ϕ!(::TestRound,::TrapQubit,::NoInputQubits,meta_graph,vertex)
-    # 
     θᵥ = get_prop(meta_graph,vertex,:init_qubit)
     rᵥ = draw_rᵥ()
     ϕ̃ = compute_angle_δᵥ(TestRound(),TrapQubit(),θᵥ,rᵥ)
