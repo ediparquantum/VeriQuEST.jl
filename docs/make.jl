b2f9593600af8bc552e134b64e3eccab05e3c353
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2024-02-13 20:06:34 +0000

    Updated doc

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,20 @@ makedocs(;
         assets=String[],
     ),
     pages=[
-        "Home" => "index.md",
+        "Introduction to VeriQuEST.jl" => "index.md",
+        "User Guide" => [
+            "Getting Started" => "user/getting_started.md",
+            "Interface with QuEST" => "user/interface_with_quest.md",
+            "Circuit Base Quantum Computing" => "user/circuit_base_quantum_computing.md",
+            "Measurement Based Quantum Computing" => "user/measurement_based_quantum_computing.md",
+            "Blind Quantum Computing" => "user/blind_quantum_computing.md",
+            "Robust Blind Verification" => "user/robust_blind_verification.md",
+            "Quantum Noise" => "user/quantum_noise.md",
+        ],
+        "References" => [
+            "C Interface functions" => "references/c_interface_functions.md",
+            "Client functionality"  => "references/client_functionality.md",
+        ]
     ],
 )
 
@@ -26,3 +39,6 @@ deploydocs(;
     branch = "gh-pages",
     versions = ["stable" => "v^", "v#.#" ]
 )
+
+
+
